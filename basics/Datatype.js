704bac9828d720b4e120574cd7086c3bf561ff58
omkar3668 <102798786+omkar3668@users.noreply.github.com>
2024-01-01 07:20:26 +0000

    basics

---
+++ basics/Datatype.js
@@ -0,0 +1,15 @@
+"usestrict";
+
+const id = 4557;
+let firstName = "omkar"
+let developerName;
+let result = null
+
+console.log(developerName);
+// console.table(id , firstName, developerName);
+
+console.log(typeof developerName);
+console.log(typeof id);
+console.log(typeof firstName);
+console.log(result);
+console.log(typeof result);
---
+++ basics/Variable.js
@@ -0,0 +1,9 @@
+// let, const, var
+// don't use var because of issue in block scope & functional scope.
+
+const empId = 3668
+let empName = "omkar"
+let empEmail = "omsable8448@gmail.com"
+let empPassword = "1234"
+
+console.table([empId ,empEmail ,empName, empPassword ]);
