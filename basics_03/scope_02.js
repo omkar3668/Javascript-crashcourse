2c0ae0070237b7b33f513367f72d29e6460a6bcc
omkar3668 <102798786+omkar3668@users.noreply.github.com>
2024-01-02 11:36:01 +0000

    scope in JS

--- basics_03/scope_02.js
+++ basics_03/scope_02.js
@@ -1,12 +1,25 @@
 //let ,const, var
-
-
+//here let & const follows the concept of block scope var not follow
 
 if (true){
 
     let a= 4;
     const b= 5;
     var c = 6;
+    console.log(a);
+    console.log(b);
 }
 
-console.log(a);
+console.log(c);
+
+
+//if we add function inside function inside function can get values from outside
+//child can get from parent but parent can't get from child.
+function addOne (num1){
+    console.log(num1 +1);
+    function addTwo(num2) {
+        console.log(num2 + num1);
+    }
+   addTwo(2);
+}
+addOne(1);
