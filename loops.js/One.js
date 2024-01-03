83d02e12325556daa9d8c9ac143b069ff6ed12d3
omkar3668 <102798786+omkar3668@users.noreply.github.com>
2024-01-03 08:15:51 +0000

    loops

---
+++ loops.js/One.js
@@ -0,0 +1,42 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//        console.log(`${element} is detected`);
+//     }
+//     console.log(element);
+// }
+
+//break
+// for (let i = 1; i < 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//        console.log(`${element} is detected`);
+//        break
+//     }
+//     console.log(element);
+// }
+
+//continue
+// for (let i = 1; i < 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//        console.log(`${element} is detected`);
+//        continue
+//     }
+//     console.log(element);
+// }
+
+ let myArr = [ "batman", "superman", "perman"]
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+   
+}
---
+++ loops.js/Two.js
@@ -0,0 +1,21 @@
+// while  loop
+// let i = 1;
+// while (i<5) {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+let myArr = ["batman", "Superman", "Aquaman"]
+let i = 0;
+while (i < myArr.length) {
+    console.log(`my favirte DC hero is ${myArr[i]} `);
+    i = i +1
+}
+
+// Doi while loop.
+let j = 5;
+
+do {
+    console.log(j);
+    j = j +1
+} while (j <= 10);
